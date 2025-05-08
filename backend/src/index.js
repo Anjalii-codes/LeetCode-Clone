5001dc14d18d43b7cd7d0f47f75f4dc4dd02ac1f
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-05-08 17:54:12 +0530

    add auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World welcome to leetcode clone ! 🔥');
+})
+app.use("/api/v1/auth",authRoutes)
 app.listen(8080, () => {
     console.log(`Server is listening on port : ${8080}`);
 });
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const authRoutes = express.Router();
+// authRoutes.post("/register", register)
+// authRoutes.post("/login", login)
+// authRoutes.post("/logout", logout)
+// authRoutes.get("/check", check)
+
+export default authRoutes;
