02027a36bea4e98817d0554b47f2f42992842de6
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-05-08 19:02:28 +0530

    add login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -60,4 +60,61 @@ export const register = async (req, res) => {
         })
         
     }
-}
+};
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    if(!email || !password) {
+        return res.status(400).json(
+            {
+                msg: "Please enter all fields"
+            })
+    }
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                email
+            }
+        })
+        if(!user) {
+            return res.status(400).json(
+                {
+                    msg: "User does not exist"
+                })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) {
+            return res.status(400).json(
+                {
+                    msg: "Invalid credentials"
+                })
+        }
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        )
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        res.status(200).json({ 
+            message: "User logged in successfully",
+            user : {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                image:user.image
+            }
+        })  
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "Something went wrong"
+
+        })
+        
+    }
+};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { register } from '../controllers/auth.controller.js';
+import { login } from '../controllers/auth.controller.js';
 const authRoutes = express.Router();
-authRoutes.post("/register", register)
-// authRoutes.post("/login", login)
+authRoutes.post("/register", register);
+authRoutes.post("/login", login)
 // authRoutes.post("/logout", logout)
 // authRoutes.get("/check", check)
 
